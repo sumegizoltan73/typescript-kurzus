c2dba360e9b86736c7912184899eecd0fe544fad
Zoltán Péter Sümegi <57563804+sumegizoltan73@users.noreply.github.com>
2025-08-22 12:12:43 +0200

    Typescript training init

---
+++ 5-ts-alapjai/src/app.ts
@@ -0,0 +1,87 @@
+/*
+let ertek = "teszt";
+let ertek2 = Math.random();
+let ertek3: Szamok = 42;
+
+type Szamok = 1 | 42 | 3.2;
+type Status = "Elkezdve" | "Folyamatban..." | "Befejezett";
+
+let progress: Status;
+
+progress = "Folyamatban...";
+
+let szamok: number[] = [];
+
+type Szemely = [name: string, age: number];
+
+let szemely1: Szemely = ["Kovács J.", 42];
+let szemely2: Szemely = ["Horváth D.", 42];
+
+const [nev, eletkor] = szemely2;
+
+function peldaFuggveny(bemenet: string | number): number {
+  if (typeof bemenet == "string") {
+    return bemenet.length;
+  }
+  return bemenet * 15;
+}
+
+console.log(peldaFuggveny("szia") *20);
+*/
+type Album = {
+  userId: number;
+  id: number;
+  title: string;
+  photos?: Array<Photo>;
+};
+
+type Photo = {
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+};
+
+async function getAlbums() {
+  const fetchedAlbums = await fetch("http://jsonplaceholder.typicode.com/albums").then(response => {
+    return response.json() as Promise<Album[]>;
+  });
+
+  const albums = fetchedAlbums.slice(0,5);
+
+  const promisesOfAllPhotos = albums.map((album) => 
+    fetch("http://jsonplaceholder.typicode.com/photos?albumId=" + album.id)
+      .then(res => res.json() as Promise<Photo[]>));
+  const allPhotos = await Promise.all(promisesOfAllPhotos);
+
+  return albums.map((album, i) => ({...album, photos: allPhotos[i]}));
+}
+
+window.onload = async function () {
+  const albums = await getAlbums() as Album[];
+  
+  render(albums);
+}
+
+function render(albumok: Array<Album>){
+  const container = document.getElementById("root");
+  if (!container) {
+    return;
+  }
+  container.innerHTML = `
+    <h1>Albumok</h1>
+    ${albumok.map(album => `
+        <div>
+          <h3>#${album.id}</h3>
+          <p>${album.title}</p>
+          <br />
+          ${album.photos?.map(photo => `
+              <img
+                src="${photo.thumbnailUrl}"
+                style="display:inline-block;margin-right: 6px;width:30px;height:30px;"
+              />
+            `).join("")}
+        </div>
+      `).join("")}
+  `;
+}
